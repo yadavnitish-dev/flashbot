0291406a0d196bc3688fa6d49496e634f83bc73c
Nitish Yadav <yadavnitish00028@gmail.com>
2026-02-04 20:17:13 +0530

    Database provider changed from neon to aiven because of connection issue

--- db/client.ts
+++ db/client.ts
@@ -1,6 +1,11 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+client.connect();
+
+export const db = drizzle(client, { schema });
