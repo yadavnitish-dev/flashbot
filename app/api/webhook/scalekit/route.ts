b8400931e4376d7f33a678ef6ecff91f37ec5a07
Nitish Yadav <yadavnitish00028@gmail.com>
2026-02-07 02:52:46 +0530

    Add new API route for processing ScaleKit webhooks, including verification and handling of organization membership events

---
+++ app/api/webhook/scalekit/route.ts
@@ -0,0 +1,47 @@
+import { db } from "@/db/client";
+import { teamMembers } from "@/db/schema";
+import scalekit from "@/lib/scalekit";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.text();
+
+    const headers = Object.fromEntries(req.headers.entries());
+
+    const secret = process.env.SCALEKIT_WEBHOOK_SECRET!;
+
+    try {
+      scalekit.verifyWebhookPayload(secret, headers, body);
+    } catch (error) {
+      console.error("Webhook verification failed:", error);
+      return NextResponse.json({ error: "Invalid signature" }, { status: 400 });
+    }
+
+    const event = JSON.parse(body);
+
+    switch (event.type) {
+      case "user.organization_membership_created":
+        const param = event.data;
+        await db
+          .update(teamMembers)
+          .set({
+            status: "active",
+          })
+          .where(eq(teamMembers.user_email, param.user.email));
+        break;
+
+      default:
+        console.log("Unhandled event type:", event.type);
+    }
+
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error("Webhook processing error:", error);
+    return NextResponse.json(
+      { error: "Failed to process webhook" },
+      { status: 500 }
+    );
+  }
+}
